c8b80dba0f412c2bdeb162db496ebfbd1a6cec52
Fredy-Cruz <fredyjcr0601@gmail.com>
2025-01-30 03:39:06 -0600

    feat: adding validations to the endpoints (in progress)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,19 +4,49 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Exception;
 
 class UserController extends Controller
 {
+    //Method to save the user
+    public function saveUser(User $user){
+        try{
+            $user->save();
+        }catch(Exception $e){
+            return response()->json([
+                "message" => "Error has ocurred"
+            ], 500);
+        }
+    }
+
+    //Method to validate searching the user by id
+    public function findUser(User $user,string $id){
+        try{
+            //Apply CRUD operations jut fot available users
+            $user = User::find($id)->where('disabled', false)->where('role', 'user');
+        }catch(Exception $e){
+            return response()->json([
+                "message" => "User not found"
+            ], 404);
+        }    
+    }
 
     //Display a listing of the resource.
     public function index()
     {
-        $users = User::where('disabled', false)->get();
+        $users = User::where('disabled', false)->where('role', 'user')->get();
+
+        //In case there are no users registered
+        if($users->isEmpty()){
+            return response()->json([
+                'message' => 'No users found'
+            ], 404);
+        }
 
         return response()->json([
             'message' => 'Get all users',
             'data' => $users
-        ]);
+        ], 200);
     }
 
 
@@ -27,16 +57,23 @@ class UserController extends Controller
         $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users,email',
-            'password' => 'required',
-/*             'birth_date' => 'required' */
+            'password' => 'required'
         ]);
+
+        //Check if the email already exists
+        $user = User::where("email", $request->email)->first();
+        if($user){
+            return response()->json([
+                "message" => "Email already exists"
+            ], 400);
+        }
 
         //Store the data
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
         $user->password = bcrypt($request->password);
-        $user->save();
+        $this->saveUser($user);
 
         return response()->json([
             'message' => 'Store data',
@@ -47,32 +84,49 @@ class UserController extends Controller
     //Display the specified resource.
     public function show(string $id)
     {
-        $user = User::find($id);
+        $user = User::find($id)->where('disabled', false)->where('role', 'user');
+        $this->findUser($user);
 
         return response()->json([
             'message' => 'Get user by id',
             'data' => $user
-        ]);
+        ], 200);
     }
 
     //Update the specified resource in storage.
-    public function update(Request $request, User $user)
+    public function update(Request $request, string $id)
     {
-        return response()->json([
-            'message' => 'Nothing to update'
+        //Only allow to update the name
+        $request->validate([
+            "name" => "required"
         ]);
-    }
 
-    //Remove the specified resource from storage.
+        $user = User::find($id)->where('disabled', false)->where('role', 'user');
+        $this->findUser($user);    
+
+        $user->name = $request->name;
+
+        $this->saveUser($user);
+        
+        return response()->json([
+            "message" => "Data Updated",
+            "data" => $user
+        ], 200);
+    }
+    
+    //Disable the specified resource from storage.
     public function destroy(string $id)
     {
-        $user = User::find($id);
-        $user->disabled = true;
-        $user->save();
+        $user = User::find($id)->where('disabled', false)->where('role', 'user');
+        $this->findUser($user);
 
+        $user->disabled = true;
+        
+        $this->saveUser($user);
+        
         return response()->json([
             'message' => 'Disabled the user',
             'data' => $user
-        ]);        
-    }
+        ], 200);        
+    } 
 }
